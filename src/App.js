938d854ca460fa60b832e8b3feeffdd4e879beca
Mitchell Foote <mcfoote321@gmail.com>
2022-12-07 23:39:17 -0500

    work on src react files

--- src/App.js
+++ src/App.js
@@ -6,4 +6,18 @@ import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
+
+
+function App() {
+
+
+    return(
+        <div>
+
+        </div>
+    );
+
+}
+
+export default App;
