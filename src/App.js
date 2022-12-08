8807ae427989839548968204697a3d90b2d0ced9
Mitchell Foote <mcfoote321@gmail.com>
2022-12-07 23:36:49 -0500

    work on src react files

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import React, { useState } from 'react';
+
+import Header from './components/Header';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer'
