14e00552a9a25bc61e8b31bf1bd9e35c1256b84c
Mitchell Foote <mcfoote321@gmail.com>
2022-12-11 23:59:17 -0500

    work on project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,9 +7,11 @@ function Project(props) {
         <section>
             <div className="columns is-justify-content-center is-flew-wrap-wrap is-desktop is-flex-direction-row">
                 {props.project.map((project) => (
-                    <div>
-                        <div>
-                            
+                    <div className="column">
+                        <div className="card">
+                            <div className="card-img">
+                                
+                            </div>
                         </div>
                     </div>
                 ))}
